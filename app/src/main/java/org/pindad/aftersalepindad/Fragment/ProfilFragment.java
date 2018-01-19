353f2102559b89bf3556cf9f476f7267df89ab7c
WIDHIYANTO NUGROHO <widhiyantonugroho10@gmail.com>
2018-01-20 02:52:55 +0700

    Layout Profil

--- app/src/main/java/org/pindad/aftersalepindad/Fragment/ProfilFragment.java
+++ app/src/main/java/org/pindad/aftersalepindad/Fragment/ProfilFragment.java
@@ -1,16 +1,22 @@
 package org.pindad.aftersalepindad.Fragment;
 
 import android.content.Context;
+import android.media.Image;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import org.pindad.aftersalepindad.R;
 
 public class ProfilFragment extends Fragment {
+
+    TextView name, email, phone, office;
+    ImageView photo, background;
 
     public ProfilFragment() {
         // Required empty public constructor
@@ -21,6 +27,18 @@ public class ProfilFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View rootView =  inflater.inflate(R.layout.fragment_profil, container, false);
+
+        background = (ImageView) rootView.findViewById(R.id.profil_background);
+        photo = (ImageView) rootView.findViewById(R.id.profil_photo);
+        name = (TextView) rootView.findViewById(R.id.profil_name);
+        office = (TextView) rootView.findViewById(R.id.profil_office);
+        email = (TextView) rootView.findViewById(R.id.profil_email);
+        phone = (TextView) rootView.findViewById(R.id.profil_phone);
+
+
+
+
+
         return rootView;
     }
 }
