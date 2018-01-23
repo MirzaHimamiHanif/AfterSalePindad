dd156178716eb9ad8a1b5039c808371eeb953827
Mirza Himami Hanif <mirzahanif7@gmail.com>
2018-01-23 09:11:34 +0700

    Profile Data.

--- app/src/main/java/org/pindad/aftersalepindad/Fragment/ProfilFragment.java
+++ app/src/main/java/org/pindad/aftersalepindad/Fragment/ProfilFragment.java
@@ -1,6 +1,8 @@
 package org.pindad.aftersalepindad.Fragment;
 
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.media.Image;
 import android.net.Uri;
 import android.os.Bundle;
@@ -10,13 +12,29 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import org.pindad.aftersalepindad.BarangActivity;
+import org.pindad.aftersalepindad.Model.DataTicketing;
+import org.pindad.aftersalepindad.Model.PostTicketing;
 import org.pindad.aftersalepindad.R;
+
+import retrofit2.Call;
 
 public class ProfilFragment extends Fragment {
 
     TextView name, email, phone, office;
     ImageView photo, background;
+    private FirebaseAuth mAuth;
+    private String mUid;
 
     public ProfilFragment() {
         // Required empty public constructor
@@ -34,11 +52,26 @@ public class ProfilFragment extends Fragment {
         office = (TextView) rootView.findViewById(R.id.profil_office);
         email = (TextView) rootView.findViewById(R.id.profil_email);
         phone = (TextView) rootView.findViewById(R.id.profil_phone);
+        mAuth = FirebaseAuth.getInstance();
+        final FirebaseUser currentUser = mAuth.getCurrentUser();
+        mUid = currentUser.getUid();
+        DatabaseReference root = FirebaseDatabase.getInstance().getReference();
+        root.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for(DataSnapshot ds : dataSnapshot.getChildren()){
+                    email.setText(ds.child(mUid).getValue(DataTicketing.class).getEmail()); //set the email
+                    name.setText(ds.child(mUid).getValue(DataTicketing.class).getNama()); //set the name
+                    phone.setText(ds.child(mUid).getValue(DataTicketing.class).getNoTelp());
+                    office.setText(ds.child(mUid).getValue(DataTicketing.class).getPerusahaan());
+                }
+            }
 
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
 
-
-
-
+            }
+        });
         return rootView;
     }
 }
